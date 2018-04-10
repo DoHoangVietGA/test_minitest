4986540df35bc80ffc0304ed3b561b0638f0ddef
Do Hoang Viet <d_h_viet@Do-Hoang-Viet.local>
2018-04-10 14:36:16 +0700

    Improve circleCI

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -6,7 +6,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { url: 'http://chrome:4444/wd/hub' }
 
   def setup
-    host! "http://127.0.0.1"
+    host! "http://#{IPSocket.getaddress(Socket.gethostname)}"
     super
   end
 end
