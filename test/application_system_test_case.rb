ef560e5a4e09d95377785606d3fd69792def31da
Do Hoang Viet <d_h_viet@Do-Hoang-Viet.local>
2018-04-10 15:51:23 +0700

    Circle ci CircleCI

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,12 +3,13 @@ require 'socket'
 require "capybara/poltergeist"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { url: 'http://chrome:4444/wd/hub' }
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]#, options: { url: 'http://chrome:4444/wd/hub' }
 
   # def setup
   #   host! "http://#{IPSocket.getaddress(Socket.gethostname)}"
   #   super
   # end
 
-  driven_by :poltergeist
+  # driven_by :poltergeist
+  driven_by :selenium_chrome_headless
 end
